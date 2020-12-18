e108aa467dcf4ae07ae4d644bfbf787bbfea0cb5
Your Name <you@example.com>
2020-12-18 15:32:01 +0000

    Tutorial Completed

---
+++ src/client-entry.js
@@ -0,0 +1,10 @@
+// client-entry.js
+import { createApp } from './app';
+
+const { app, router } = createApp({ state: window.__INITIAL_STATE__ });
+
+import './assets/style.scss';
+
+router.onReady(() => {
+  app.$mount('#app');
+});
